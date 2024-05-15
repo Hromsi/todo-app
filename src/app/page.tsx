f49c17ca44f1936d6284a814ac86c3bc9936d6e4
Hromsi <bodak2011@yandex.ru>
2024-05-15 21:19:27 +0300

    Created simple todo list app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,65 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+
+import styles from './page.module.css';
+import { useState } from 'react';
+
+interface ITodo {
+	id: string,
+	text: string,
+	completed: boolean,
+}
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [text, setText] = useState('');
+	const [todos, setTodos] = useState<ITodo[]>([]);
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	const addTodo = (text: string) => {
+		const newTodo = {
+			id: new Date().toISOString(),
+			text: text,
+			completed: false,
+		};
+		setText('');
+		setTodos(prev => [...prev, newTodo]);
+	};
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+	const toggleTodoComplete = (id: string) => {
+		setTodos(todos.map(todo => todo.id === id? { ...todo, completed:!todo.completed } : todo
+		));
+	};
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+	const removeTodo = (id: string) => {
+		const newTodos = todos.filter(todo => todo.id !== id);
+		setTodos(newTodos);
+	};
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
-}
+	return (
+		<main className={styles.main}>
+			<div className="App">
+				<label>
+					<input 
+						placeholder="new todo"
+						value={text} 
+						onChange={e => setText(e.target.value)}
+					/>
+					<button onClick={() => addTodo(text)}>Add Todo</button>
+				</label>
+				<ul>
+					{todos.map((todo) => (
+						<li key={todo.id}>
+							<input 
+								type="checkbox" 
+								checked={todo.completed} 
+								onChange={() => toggleTodoComplete(todo.id)}
+							/>
+							<span>{todo.text}</span>
+							<span className="delete" onClick={() => removeTodo(todo.id)}>
+								&times;
+							</span>
+						</li>
+					))}
+				</ul>
+			</div>
+		</main>
+	);
+} 
